a551c5edf1b2600adf4a197eb0b2553e9e4bd5b1
Deepthi21GIT <95394886+Deepthi21GIT@users.noreply.github.com>
2021-12-18 12:18:01 -0600

    Add files via upload

---
+++ chap01ex.py
@@ -0,0 +1,57 @@
+from __future__ import print_function, division
+import sys
+import nsfg
+import thinkstats2
+
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+
+    pass
+
+
+def ValidatePregnum(resp):
+
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+       # print(caseid, len(indices), pregnum)
+    return True
+
+
+def main(script):
+
+    resp = ReadFemResp()
+
+    assert (len(resp) == 7643)
+    assert (resp.pregnum.value_counts()[1] == 1267)
+    assert (ValidatePregnum(resp))
+
+    print('%s: All tests passed.' % script)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
